719c06041c669d3d621807f080d4ea7ca0d4efae
bwgjoseph <josephgan@live.com.sg>
2023-09-03 16:51:23 +0800

    add maven-101-basics project

---
+++ maven-101-basics/README.md
@@ -0,0 +1,3 @@
+# Maven 101 - Basics
+
+See this [blogpost](https://bwgjoseph.com/maven-101-basics) for detailed explanation
---
+++ maven-101-basics/src/main/java/com/bwgjoseph/maven101basics/Maven101BasicsApplication.java
@@ -0,0 +1,9 @@
+package com.bwgjoseph.maven101basics;
+
+public class Maven101BasicsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting application...");
+	}
+
+}
